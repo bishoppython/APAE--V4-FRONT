e6783069c4a81e01d1967c7d408926b37a39dd2a
Dev-Nadson <nadson.pros@gmail.com>
2026-04-12 23:24:35 -0300

    feat: volume icon added

--- src/components/ui/necessity_card.tsx
+++ src/components/ui/necessity_card.tsx
@@ -1,14 +1,26 @@
 import { cn } from '@/libs/utils';
+import { Volume2 } from 'lucide-react';
 import * as React from 'react';
 
 export function NecessityCardContainer({ children }: React.ComponentProps<'button'>) {
-    return <button className={cn(
-        'size-80 rounded-[1.25rem] flex flex-col items-center justify-between hover:scale-110 cursor-pointer',
-        'focus:outline-none focus:ring-3 focus:ring-secondary/60 transition-all duration-300',
-    )}
-    >
-        {children}
-    </button>
+    return (
+        <div className='relative hover:scale-105 cursor-pointer focus:outline-none focus:ring-3 focus:ring-secondary/60 transition-all duration-300'>
+            <button className={cn(
+                'size-60 rounded-[1.25rem] flex flex-col items-center justify-between',
+            )}
+            >
+                {children}
+            </button>
+
+            <span
+                className={cn(
+                    "bg-primary text-white font-extrabold absolute top-7 -translate-y-1/2 right-0 p-2.5 rounded-2xl mr-2"
+                )}
+            >
+                <Volume2 className="size-5 stroke-3" />
+            </span>
+        </div>
+    )
 }
 
 export function NecessityCardImage({ src, alt }: React.ComponentProps<'img'>) {
@@ -18,7 +30,7 @@ export function NecessityCardImage({ src, alt }: React.ComponentProps<'img'>) {
 }
 
 export function NecessityCardTitle({ children }: React.ComponentProps<'h1'>) {
-    return <div className='w-full bg-primary text-white font-semibold text-center rounded-b-[1.25rem] h-15 flex items-center justify-center text-2xl'>
+    return <div className='w-full bg-primary text-white font-semibold text-center rounded-b-[1.25rem] h-12 flex items-center justify-center text-xl'>
         {children}
     </div>
 }
