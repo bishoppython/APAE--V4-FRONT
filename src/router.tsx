2e4c2f85bbecaaf8dcbbd85d1f58332a6ed5b1a8
Mayron Wilke <mayronwilke28@gmail.com>
2026-04-13 13:36:20 -0300

    Labiririnto
    labirinto em react

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/home";
+import Labirinto from "./pages/jogos/Labirinto";
 
 export function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/labirinto" element={<Labirinto />} />
             </Routes>
         </BrowserRouter>
     );
