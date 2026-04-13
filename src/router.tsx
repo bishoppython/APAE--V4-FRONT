c73acae5e9f8008ee21dae29d530d688268a85e5
Dev-Nadson <nadson.pros@gmail.com>
2026-04-12 23:24:52 -0300

    feat: necessity page initialized

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/home";
+import { Necessities } from "./pages/necessities";
 
 export function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/necessities" element={<Necessities />} />
             </Routes>
         </BrowserRouter>
     );
