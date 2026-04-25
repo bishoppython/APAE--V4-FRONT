a549ef5d96a53cf9188c8967da59e254ed5bcb0f
Mayron Wilke <89049286+Mayron28br@users.noreply.github.com>
2026-04-25 00:12:27 -0300

    Revert "Migração Jogo Adivinha Animais (#21)" (#22)
    This reverts commit 114adc2138d6997d8ed44dae863b65da21e68a8a.

--- src/router.tsx
+++ src/router.tsx
@@ -18,7 +18,6 @@ import { Colors } from "@/pages/conversation/colors";
 import Soletrando from "@/pages/jogos/Soletrando";
 import Labirinto from "@/pages/jogos/Labirinto";
 import Memoria from "@/pages/jogos/Memoria";
-import AdivinhaAnimais from "./pages/jogos/AdivinhaAnimais";
 
 function MainLayout() {
     return (
@@ -46,9 +45,8 @@ export function AppRouter() {
                     <Route path="/calculadora" element={<Numbers />} />
                     <Route path="/animais" element={<Animals />} />
                     <Route path="/misturando-cores" element={<ColorMixer />} />
-                    
+
                     {/* Jogos */}
-                    <Route path="/adivinha-animais" element={<AdivinhaAnimais />} />
                     <Route path="/labirinto" element={<Labirinto />} />
                     <Route path="/soletrando" element={<Soletrando />} />
                     <Route path="/memoria" element={<Memoria />} />
